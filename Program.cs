2545d309ead78df10bb078a0a43dc036ed2e68a3
Kevin Castro <kevin.castro.chavez0@gmail.com>
2024-05-10 22:31:41 -0600

    learned about numbers

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ class Program
         // Console.WriteLine("This is the first line.");
         // Console.WriteLine("This is the second line.");
         // Console.WriteLine('a');
-        Console.WriteLine('This will throw an error.');
+        // Console.WriteLine('This will throw an error.');
+        Console.WriteLine(123); // For numbers
+        Console.WriteLine(0.25F); // For floats
     }
 }
--- obj/Debug/net8.0/FirstApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/FirstApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+863a010c50b89d6cc195815efc6962bf3e382267")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c4fbc953ea92a40ce64a7de93a3291b0624666a")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
