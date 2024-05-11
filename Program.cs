61feb5c5c180250c3e177ec510fb30d79ff17016
Kevin Castro <kevin.castro.chavez0@gmail.com>
2024-05-10 22:55:39 -0600

    performed basic string formatting

--- Program.cs
+++ Program.cs
@@ -28,9 +28,29 @@ class Program
 
         // var message = "Hello world!"; // var is used for dynamic typing
 
+        // string firstName = "Bob";
+        // int messages = 3;
+        // double temperature = 34.4; // or decimal temperature = 34.4m;
+        // Console.WriteLine($"Hello, {firstName}! You have {messages} messages in your inbox. The temperature is {temperature} celsius.");
+
+        Console.WriteLine("Hello\nWorld!"); // \n for new line
+        Console.WriteLine("Hello\tWorld!"); // \t for tab
+        Console.WriteLine("Hello \"World\"!"); // \ for escape
+        Console.WriteLine("c:\\source\\repos"); // \\ since one is for escape
+        Console.WriteLine(@"    c:\source\repos    
+        (this is where your code goes)");
+
         string firstName = "Bob";
-        int messages = 3;
-        double temperature = 34.4; // or decimal temperature = 34.4m;
-        Console.WriteLine($"Hello, {firstName}! You have {messages} messages in your inbox. The temperature is {temperature} celsius.");
+        string message = "Hello " + firstName;
+        Console.WriteLine(message);
+
+        string projectName = "ACME";
+        string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
+
+        Console.WriteLine($@"View English output:    
+        c:\Exercise\{projectName}\data.txt");
+
+        Console.WriteLine($@"View English output:    
+        c:\Exercise\{russianMessage}\data.txt");
     }
 }
